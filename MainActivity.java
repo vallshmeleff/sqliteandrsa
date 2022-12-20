2e90b34fa5f6a553d18e89b8cc1ce8081a7b2523
Valery Shmelev <114564020+vallshmeleff@users.noreply.github.com>
2022-12-20 10:10:10 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -649,6 +649,47 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
 
+        // ============================================================
+    //
+    // This method takes 5 strings and generates one <shmelev> delimited
+    // new string from them. New line is placed in one field of a SQLite database record
+    //
+    // In the following method, the string is divided by the <shmelev> tag into its component parts
+    // (pseudo-fields of the database record)
+    //
+    // v 1.0
+    //
+    // ============================================================
+    public String textBoost(String e1Text, String e2Text, String e3Text, String e4Text, String e5Text) { // TEXT fragments
+        String nText ="";
+        nText = "<shmelev>" + e1Text + "<shmelev>" + e2Text + "<shmelev>" + e3Text + "<shmelev>" + e4Text + "<shmelev>" + e5Text;
+            // SQLite Booster (c) by Valery Shmelev https://www.linkedin.com/in/valery-shmelev-479206227/
+        Log.d("= Boosted text =","== == == == == ==| BOOSTED |== == == == " +  nText);
+        return nText; //Return Text array - Fragments array
+
+    } // textBoost()
+
+
+    // ============================================================
+    //
+    // Restored Text Array
+    //
+    // v 1.0
+    //
+    // ============================================================
+    public String[] unBooster(String unBoosted) {
+        // ============================================================
+        // Convert Boosted Text to unBoosted (Splitter)
+        // ============================================================
+        String substr = unBoosted.substring(0,9);
+        String[] splitted = unBoosted.split("<shmelev>"); // Split encrypted string by delimiter and write to array
+            Log.d("= unBoosted text =", "== == == == == ==| UNBOOSTED |== == == == " + unBoosted);
+            Log.d("= unBoosted SUBSTRING =", "== == == == == ==| SUBSTRING |== == == ==" + substr);
+        return splitted; //Return Text array
+    }
+
+    
+    
 
 
 
